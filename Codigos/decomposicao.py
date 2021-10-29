19b23cf98dc4cd64e0a8a14e845227b0e4b6c9fc
Augusto Shimizu <augustohss@gmail.com>
2021-10-29 20:27:03 -0300

    New decomposition

--- Codigos/decomposicao.py
+++ Codigos/decomposicao.py
@@ -11,8 +11,8 @@ plt.rcParams['figure.figsize'] = [12, 9]
 df_data = read_csv('../DatasetsTratados/processed_data.csv', sep=',', index_col='Updated')
 df_data.index = to_datetime(df_data.index)
 
-startDate =  df_data.index.min()
-endDate = to_datetime('2021-10-01', format='%Y-%m-%d') # df_data.index.max()
+startDate =  to_datetime('2021-07-01', format='%Y-%m-%d')
+endDate = to_datetime('2022-10-01', format='%Y-%m-%d')
 # endDate = df_data.index.max()
 
 df_tofilterstart = df_data.index >= startDate
@@ -50,7 +50,7 @@ argentina_recovered = decompose(df_filtered, 'ARG', 'Recovered')
 argentina_deaths = decompose(df_filtered, 'ARG', 'Deaths')
 
 def baseFuncLog(x, a, b):
-  return a * np.log(x) + b
+  return (a * x) + b
 
 def nonLinearRegression(data, func, title):
   for i in range(len(data)):
@@ -65,35 +65,40 @@ def nonLinearRegression(data, func, title):
         data[i] = aux
       else: data[i] = data[i-1]
       
-  xdata = np.linspace(minDate.value, maxDate.value, len(data))
-  print(xdata)
+  # xdata = np.linspace(minDate.value, maxDate.value, len(data))
   
   xdataInTime = np.linspace(minDate.value, maxDate.value, len(data))
   xdataInTime = to_datetime(xdataInTime)
-  teste = np.polyfit(np.log(xdata), data, 1)
 
+  data = data / 1000
+
+  aux = np.linspace(1, len(data), len(data))
+  teste = np.polyfit(aux, data, 1)
+
+  print(teste)
+  
   # popt, pcov = curve_fit(func, xdata, data, p0=[1,1,1],bounds=[1,160000000])
 
   plt.plot(xdataInTime, data, 'b-', label='Tendência')
 
-  plt.plot(xdataInTime, func(xdata, teste[0], teste[1]), 'r-',
-          label='Ajuste: a=%5.3f, b=%5.3f' % tuple(teste))
+  plt.plot(xdataInTime, func(aux, teste[0], teste[1]), 'r-',
+          label='Ajuste: a=%5.5f, b=%5.3f' % tuple(teste))
 
-  plt.xlabel('Data de atualização')
-  plt.ylabel('Quantidade')
+  plt.xlabel('Data de atualização (1:10e3)')
+  plt.ylabel('Quantidade (1:10e3)')
   plt.title(title)
   plt.legend()
   plt.show()
 
 
-nonLinearRegression(worlwide_confirmed.trend, baseFuncLog, 'Função de ajuste: a log(x) + b - Casos Confirmados - Mundial')
-nonLinearRegression(worlwide_recovered.trend, baseFuncLog, 'Função de ajuste: a log(x) + b - Recuperados - Mundial')
-nonLinearRegression(worlwide_deaths.trend, baseFuncLog, 'Função de ajuste: a log(x) + b - Mortes - Mundial')
+nonLinearRegression(worlwide_confirmed.trend, baseFuncLog, 'Função de ajuste: ax + b - Casos Confirmados - Mundial')
+nonLinearRegression(worlwide_recovered.trend, baseFuncLog, 'Função de ajuste: ax + b - Recuperados - Mundial')
+nonLinearRegression(worlwide_deaths.trend, baseFuncLog, 'Função de ajuste: ax + b - Mortes - Mundial')
 
-nonLinearRegression(brazil_confirmed.trend, baseFuncLog, 'Função de ajuste: a log(x) + b - Casos Confirmados - Brasil')
-nonLinearRegression(brazil_recovered.trend, baseFuncLog, 'Função de ajuste: a log(x) + b - Recuperados - Brasil')
-nonLinearRegression(brazil_deaths.trend, baseFuncLog, 'Função de ajuste: a log(x) + b - Mortes - Brasil')
+nonLinearRegression(brazil_confirmed.trend, baseFuncLog, 'Função de ajuste: ax + b - Casos Confirmados - Brasil')
+nonLinearRegression(brazil_recovered.trend, baseFuncLog, 'Função de ajuste: ax + b - Recuperados - Brasil')
+nonLinearRegression(brazil_deaths.trend, baseFuncLog, 'Função de ajuste: ax + b - Mortes - Brasil')
 
-nonLinearRegression(argentina_confirmed.trend, baseFuncLog, 'Função de ajuste: a log(x) + b - Casos Confirmados - Argentina')
-nonLinearRegression(argentina_recovered.trend, baseFuncLog, 'Função de ajuste: a log(x) + b - Recuperados - Argentina')
-nonLinearRegression(argentina_deaths.trend, baseFuncLog, 'Função de ajuste: a log(x) + b - Mortes - Argentina')
+nonLinearRegression(argentina_confirmed.trend, baseFuncLog, 'Função de ajuste: ax + b - Casos Confirmados - Argentina')
+nonLinearRegression(argentina_recovered.trend, baseFuncLog, 'Função de ajuste: ax + b - Recuperados - Argentina')
+nonLinearRegression(argentina_deaths.trend, baseFuncLog, 'Função de ajuste: ax + b - Mortes - Argentina')
